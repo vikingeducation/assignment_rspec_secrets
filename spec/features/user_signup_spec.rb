20bcfcd3f15d0370b510f1c0f3b779152fdf3a4a
Alice Fung <alice.o.fung@gmail.com>
2015-08-07 12:53:58 -0400

    add feature testing view secrets

---
+++ spec/features/user_signup_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+#sign up
+#should be able to sign up as new user
+#should not be able to sign up as new user when already logged in
+feature 'sign up new user' do
+
+  before do
+    # create(:secret)
+    visit root_path
+  end
+
+  scenario 'should add a new user' do
+    click_link "All Users"
+    click_link "New User"
+    name = "tester"
+    fill_in "Name", with: name
+    fill_in "Email", with: "test@test.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    expect{click_button "Create User"}.to change(User, :count).by(1)
+    expect(page).to have_content("Welcome, #{name}")
+    expect(page).to have_content("User was successfully created.")
+  end
+end
---
+++ spec/features/view_secrets_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+feature 'visiting user' do
+
+  before do
+    create(:secret)
+    visit root_path
+  end
+
+  scenario 'can view list of secrets' do
+    expect(page).to have_content("Listing secrets")
+  end
+
+  scenario 'can see list of all users' do
+    click_link "All Users"
+    expect(page).to have_content("Listing users")
+  end
+
+  scenario 'cannot see the author of the secret' do
+    expect(page).to have_content("**hidden**")
+    # within_table () do
+    #   expect(page).to have_content("**hidden**")
+    # end
+    # table = find(:xpath, '//table/tbody')
+    # binding.pry
+    # expect(table).to have_content("**hidden**")
+  end
+
+end
+
+
+
+#sign in
+#existing user can sign in
+#non-existing user cannot sign in
+#can view author once signed in
+#expect my name to be displayed on page with welcome msg
+#can log out
+
+#creating secrets
+#signed in user can create secret
+#non-signed in user cannot create secret
+
+#editing secrets
+#signed in user can edit their own secret
+#signed in user cannot edit someone else's secret
+#visitor cannot edit anyone's secret
+
+#deleting
+#signed in user can delete their own secret
+#signed in user cannot delete someone else's secret
+#visitor cannot delete anyone's secret
