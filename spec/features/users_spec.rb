db684b244e5bac535bba256e15ad4a78b821a9e5
alokp <alokpra@gmail.com>
2015-08-07 10:01:39 -0700

    working on user edits secrets tests

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -78,14 +78,89 @@ feature 'user interaction' do
       expect(page).to have_content("Welcome, #{@u.name}")
     end
 
-    # not done
-    scenario 'user should not be able to sign in with their credentials' do
+    scenario 'user should not be able to sign in with wrong credentials' do
+      fill_in 'email', with: "wrong@email.com"
+      fill_in 'password', with: @pwd
+      click_button 'Log in'
+      expect(page).to_not have_content("Welcome, #{@u.name}")
+    end
+
+    scenario 'user should not be able to sign in if not registered' do
+      @u.destroy
       fill_in 'email', with: @email
       fill_in 'password', with: @pwd
       click_button 'Log in'
-      expect(page).to have_content("Welcome, #{@u.name}")
+      expect(page).to_not have_content("Welcome, #{@u.name}")
+    end
+  end
+
+  # As a signed-in user, I want to be able to create a secret
+  context 'as a signed in user, I can create a secret' do
+
+    before do
+      @u = create(:user)
+      visit new_session_path
+      fill_in 'email', with: @u.email
+      fill_in 'password', with: @u.password
+      click_button 'Log in'
+      click_link 'New Secret'
+    end
+  
+    scenario 'should not be brought to login page' do
+      expect(current_url).to_not eq(new_session_url)
     end
 
+    scenario 'new secret page does render' do
+      expect(current_url).to eq(new_secret_url)
+    end
+
+    #happy path
+    scenario 'entering valid information creates a secret' do
+      fill_in 'secret_title', with: "New Secret"
+      fill_in 'secret_body', with: "New secret body"
+      expect{click_button 'Create Secret'}.to change(Secret, :count).by(1)
+      expect(current_url).to eq(secret_url(Secret.last))
+    end
+
+    #sad path
+    scenario 'entering invalid information gives errors' do
+      fill_in 'secret_title', with: "N"
+      fill_in 'secret_body', with: "1"*141
+      expect{click_button 'Create Secret'}.to change(Secret, :count).by(0)
+      expect(current_url).to eq(secrets_url)
+      expect(page).to have_css('#error_explanation')
+    end
 
   end
-end
+
+  context 'without signing in I can not create a secret' do
+
+    before do
+      visit root_path
+      click_link 'New Secret'
+    end
+    
+    scenario 'should be brought to login page' do
+      expect(current_url).to eq(new_session_url)
+    end
+
+    scenario 'new secret page does not render' do
+      expect(current_url).to_not eq(new_secret_url)
+    end
+
+  end
+
+  # As a signed-in user, I want to be able to edit one of my secrets
+
+  end
+
+
+
+
+
+
+
+
+
+
+
