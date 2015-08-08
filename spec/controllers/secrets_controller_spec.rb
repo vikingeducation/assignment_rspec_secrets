74e558116c962269a881d9f23e73463cf8e8194c
alokp <alokpra@gmail.com>
2015-08-08 10:55:56 -0700

    working on secret update

--- spec/controllers/secrets_controller_spec.rb
+++ spec/controllers/secrets_controller_spec.rb
@@ -26,6 +26,46 @@ describe SecretsController do
 
     end
 
+    describe 'GET #edit' do
+
+      it 'renders the right page' do
+        get :edit, :id => secret.id
+        expect(response).to render_template(:edit)
+      end
+
+      it 'does not render for a user not owning the secret' do
+        secret2 = create(:secret)
+        expect{get :edit, id: secret2.id}.to raise_error(ActiveRecord::RecordNotFound)
+      end
+
+    end
+
+    describe 'PUT #update' do
+
+      it 'can be changed by owner' do
+        put :update,  id: secret.id,
+                      secret: attributes_for(
+                              :secret, 
+                              title: "new_title")
+        secret.reload
+        expect(secret.title).to eq("new_title")
+      end
+
+      it 'cannot be changed by a person other than owner' do
+        # # user2 = create(:user)
+        # secret2 = create(:secret)
+        # # binding.pry
+        # get :edit, id: secret.id
+        # # post :update,  id: secret2.id,
+        # #               secret: attributes_for(
+        # #                       :secret, 
+        # #                       title: "new_title")
+        # # secret2.reload
+        # expect(response).to redirect_to(new_session_path)
+      end
+
+    end
+
   end
 
 end
