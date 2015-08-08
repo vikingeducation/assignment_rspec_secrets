3e3adb63836a6173e3106e3ef395e493dc0e90b6
David <davidmeza@outlook.com>
2015-08-08 16:52:19 -0400

    finish view testing

--- spec/views/shared/navigation_spec.rb
+++ spec/views/shared/navigation_spec.rb
@@ -6,18 +6,26 @@ describe "shared/_navigation.html.erb" do
 	let(:user) { create(:user) }
 
 	before do
-		assign(:user, user)	
-		def view.current_user 
+		assign(:user, user)
+		def view.current_user
 			@user
 		end
 	end
 
-  specify "a logged in user can see a logout link" do 
+  specify "a logged in user can see a logout link" do
     def view.signed_in_user?
       true
     end
     render
     expect(rendered).to match("Logout")
+  end
+
+  specify "a logged out user can see a login link" do
+    def view.signed_in_user?
+      false
+    end
+    render
+    expect(rendered).to match("Login")
   end
 
 end
