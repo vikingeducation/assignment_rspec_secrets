1e7c90e73c002c11ab4d21825a281afe0cb72186
Adam <adam.kinson@gmail.com>
2015-08-10 08:33:44 -0400

    continue on integration test

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -100,11 +100,94 @@ feature 'Sign up flow' do
 end
 
 
-feature 'User accounts' do
+feature 'Signing in and out' do
 
-  # As a not-signed-in user, I want to sign in to my account
+  let(:user) { create(:user) }
+
+  before do
+    visit root_path
+  end
+
+
+  scenario 'visitor can sign in with valid info' do
+    click_link "Login"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    expect(page).to have_content "Welcome, #{user.name}"
+  end
+
+
+  scenario 'sign in with invalid info renders' do
+    click_link "Login"
+    fill_in "Email", with: 'bad_email@example.com'
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    expect(page).to have_content "Login"
+  end
+
+
+  scenario 'sign out returns to root' do
+    click_link "Login"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    click_link "Logout"
+    expect(page).to have_content "Login"
+  end
+
+end
+
+
+feature 'User making secrets' do
+
+  let(:user) { create(:user) }
+
+  before do
+    visit new_session_path
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+  end
+
+
   # As a signed-in user, I want to be able to create a secret
+  scenario 'user can create a secret' do
+    click_link "New Secret"
+    title = "Foo title"
+    body = "Foo body"
+
+    fill_in "secret_title", with: title
+    fill_in "secret_body", with: body
+    click_button "Create Secret"
+
+    expect(page).to have_content "successfully created"
+    expect(page).to have_content title
+    expect(page).to have_content body
+  end
+
+
   # As a signed-in user, I want to be able to edit one of my secrets
+  scenario 'user can edit a secret' do
+    # create secret for user
+    create(:secret, :author_id => user.id)
+    visit secrets_path
+
+    expect(page).to have_link('Edit', :href => "/secrets/#{user.id}/edit")
+    # Edit links should include user.id in the href
+    #find_link('Edit', :href => "/secrets/#{user.id}/edit" ).click
+
+  end
+
+
+
   # As a signed-in user, I want to be able to delete one of my secrets
+  scenario 'user can delete a secret' do
+
+  end
+
 
 end
