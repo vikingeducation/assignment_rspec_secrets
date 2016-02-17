f77bc59731c0dcadb13af4adc70e2321bfdca133
koziscool <kozjob@yahoo.com>
2016-02-17 11:55:47 -0600

    secret tests pass

--- spec/factories.rb
+++ spec/factories.rb
@@ -15,5 +15,21 @@ FactoryGirl.define do
                     #doesn't have toi be build
   end
 
+  factory :secret_wo_title, class: :secret do
+    body "Secret body"
+    author       
+  end
+
+  factory :secret_wo_body, class: :secret do
+    title "Secret body"
+    author 
+  end
+
+  factory :multiple_secrets, class: :secret do
+    sequence(:title) { |n| "Secret title#{n}" }
+    sequence(:body) { |n| "Secret body#{n} lorem ipsum" }
+    
+    author  
+  end
 
 end
--- spec/models/secret_spec.rb
+++ spec/models/secret_spec.rb
@@ -1,12 +1,5 @@
 
 
-# describe Secret do
-#   it must have author
-#   it must have title, 4 .. 24
-#   it must have body 4 .. 140
-   # ...
-# end
-
 require 'rails_helper'
 
 describe Secret do
@@ -16,21 +9,46 @@ describe Secret do
   # calling `build` using `FactoryGirl.build`
 
   let(:our_secret){ build(:secret) }
+  let(:secret_without_title){ build(:secret_wo_title) }
+  let(:secret_without_body){ build(:secret_wo_body) }
 
   it "is valid with default attributes" do
     expect(our_secret).to be_valid
   end
 
-  it "is valid with default attributes" do
+  it "secret author should be User" do
     expect(our_secret.author).to be_instance_of( User )
   end
 
-  it "is valid with default attributes" do
+  it "secret title exists with proper length" do
     expect(our_secret.title.length).to be_within(10.1).of(14.0)
   end
 
-  it "is valid with default attributes" do
+  it "secret body exists with proper length" do
     expect(our_secret.body.length).to be_within(68.1).of(72.0)
   end
 
+  it "is not valid without title" do
+    expect(secret_without_title).to_not be_valid
+  end
+
+  it "is not valid without body" do
+    expect(secret_without_body).to_not be_valid
+  end
 end
+
+describe "last five" do
+  let(:secrets)  do
+    NUM_SECRETS = 10
+    create_list(:multiple_secrets, NUM_SECRETS) 
+    Secret.last_five
+  end
+
+  it "returns 5 elts of class Secret" do
+    expect(secrets[0]).to be_instance_of( Secret )
+    expect(secrets.length).to eq( 5 )
+  end
+
+end
+
+
