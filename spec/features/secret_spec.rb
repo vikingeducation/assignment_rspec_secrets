69e96104e8e7735af15b860a587e8f140d3047d0
Sam O'Keefe <samok13@gmail.com>
2016-02-18 11:54:27 -0800

    finished tests for now

--- spec/features/secret_spec.rb
+++ spec/features/secret_spec.rb
@@ -4,66 +4,41 @@ require 'rails_helper'
 
 feature 'view secrets as a visitor' do
 
-  before do
-    visit root_path
-  end
-
+  let(:secret){ create(:secret) }
+  
   scenario 'secrets table is visible' do
+    visit root_path
+
     expect(page).to have_css('body > table > thead > tr > th')
     expect(page).to have_content('Title')
     expect(page).to have_content('Body')
     expect(page).to have_content('Author')
   end
 
-end
-
-
-feature 'logging in' do
-
-  let(:user){ create(:user, :email => 'foo0@bar.com', :password => 'foobar') }
-
-  before do
+  scenario 'individual secrets are viewable' do
+    secret
     visit root_path
+    click_link('Show')
+
+    expect(page).to have_content(secret.title)
+    expect(page).to have_content(secret.body)
+
+    expect(page).to have_link('Back', secrets_path)
+
   end
 
-  scenario 'login button is present' do
-    expect(page).to have_content('Login')
-    expect(page).to have_selector("a[href='/session/new']")
+  scenario 'user can not edit or delete secrets' do
+    secret
+    visit root_path
 
-    # mention in group scrum:
-    expect(page).to have_link("Login", :href => new_session_path)
+    expect(page).to_not have_link('Edit', :href => edit_secret_path(secret))
+    expect(page).to_not have_link('Delete', :href => secret_path(secret))
   end
 
-  scenario 'visitor is able to login' do
-    user
-
-    log_in(user)
-
-    expect(page).to have_content('Username')
-    expect(page).to_not have_content('hidden')
-  end
 end
 
 
-feature 'signing up' do
 
-before do
-  visit users_path
-end
-
-  scenario 'vistor is able to sign up' do
-    click_on('New User')
-
-    fill_in('Name', :with => 'Julia')
-    fill_in('Email', :with => 'julia@bar.com')
-    fill_in('Password', :with => 'foobar')
-    fill_in('Password confirmation', :with => 'foobar')
-
-     click_on('Create User')
-
-     expect(page).to have_content('User was successfully created.')
-  end
-end
 
 feature 'creating a new secret' do
 
@@ -86,7 +61,7 @@ end
 
 feature 'editing a secret' do
 
-   let(:secret){ create(:secret) }
+  let(:secret){ create(:secret) }
 
   scenario 'logged in user can edit their secrets' do
     secret
@@ -99,10 +74,43 @@ feature 'editing a secret' do
      expect(page).to have_content('Secret was successfully updated.')
 
      expect(page).to have_content('This is an edited secret body.')
+    end
+
+  scenario 'logged in user cannot add invalid body and title' do
+    secret
+    log_in(secret.author)
+
+    visit edit_secret_path(secret)
+    fill_in('Title', :with => 'title'*5)
+    fill_in('Body', :with => 'hi')
+    click_on('Update Secret')
+
+    expect(page).to have_content('Title is too long ')
+    expect(page).to have_content('Body is too short ')
+  end
 
   end
 
+feature 'deleting a secret' do
+
+  let(:secret){ create(:secret) }
+
+  scenario 'logged in user can delete their secrets' do
+    secret
+    log_in(secret.author)
+
+    visit secrets_path
+    
+    click_on('Destroy')
+
+     expect(page).to_not have_content(secret.title)
+     expect(page).to_not have_content(secret.body)
+    
+  end
+
 end
+
+
 
 
 
