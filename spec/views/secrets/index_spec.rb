f4ae4c2958922c62b7170d8c51d4008644577579
Julia Herron <julia.herron@gmail.com>
2016-02-19 10:38:39 -0800

    starting view tests

---
+++ spec/views/secrets/index_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+describe 'secrets/index.html.erb' do
+
+  let(:secret) { create(:secret) }
+
+  before :each do
+    def view.signed_in_user?
+      false
+    end
+    @secret = secret
+    def view.current_user
+      @secret.author
+    end
+  end
+
+  it 'does not show secret author for visitor' do
+    assign(:secrets, [secret])
+    
+    render
+    expect(rendered).to have_content("hidden")
+  end
+
+
+
+
+
+
+
+end
+
+
+
+# # this line tells spec which template to render
+# describe "users/index.html.erb" do
+
+#   it "shows the User's first name" do
+
+#     # get ready to set our instance variable
+#     user = create(:user)
+#     users = [user, create(:user)]
+
+#     # Actually set the instance variable.
+#     # This is identical to writing `@users = users`
+#     #   but is more "RSpec-like"
+#     assign(:users, users) 
+
+#     # render the view
+#     render
+
+#     # Check that it contains our user's first name
+
+#     # ... HTML style
+#     expect(rendered).to match('<h1>users index</h1>')
+
+#     # ... CSS style
+#     expect(rendered).to have_selector("a[href=\"#{user_path(user)}\"]", :text => "Show #{user.name}")
+#   end
+# end
