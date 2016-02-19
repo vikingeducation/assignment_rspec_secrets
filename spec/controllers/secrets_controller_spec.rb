0a105897e0f2ffc8594948bb0e0ca04dbd70720a
Thomas Lo <thomasjinlo@gmail.com>
2016-02-19 10:02:54 -0800

    Add destroy tests

--- spec/controllers/secrets_controller_spec.rb
+++ spec/controllers/secrets_controller_spec.rb
@@ -8,6 +8,7 @@ describe SecretsController do
   let(:updated_title) { "This is a new title" }
 
   before :each do
+    secret
     session[:user_id] = user.id
   end
 
@@ -23,6 +24,7 @@ describe SecretsController do
 
 
   describe 'PUT#update' do
+
     it 'user can edit their own secret' do
       secret
       put :update, id: secret.id, secret: attributes_for(:secret, title: updated_title)
@@ -30,12 +32,42 @@ describe SecretsController do
       expect(secret.title).to eq(updated_title)
     end
 
-
-
-  end
-
-
-
-
+    it 'redirects to the updated user' do
+      secret
+      put :update, id: secret.id, secret: attributes_for(:secret, title: updated_title)
+      expect(response).to redirect_to assigns(:secret)
+    end
 
   end
+
+
+  describe 'GET#edit' do
+
+    it 'works normal for this user' do
+      get :edit, id: secret.id
+      expect(response).to render_template :edit
+    end
+
+    it 'denies access for unauthorized user' do
+      another_secret = create(:secret)
+      expect { get :edit, id: another_secret.id }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  describe 'delete#Destroy' do
+
+    it 'destroys a secret' do
+      expect { delete :destroy, id: secret.id }.to change {Secret.count }.by(-1)
+    end
+
+    it 'redirects to the secrets_url' do
+      delete :destroy, id: secret.id
+      expect(response).to redirect_to secrets_url
+    end
+
+    it 'does not allow non-signed in user to destroy secret' do
+      session[:user_id] = nil
+      expect { delete :destroy, id: secret.id }.to change {Secret.count }.by(0)
+    end
+  end
+end
