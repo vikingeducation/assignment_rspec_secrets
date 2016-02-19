ef4c11311fa0cafe529f924b886775700ddcbd49
Sia <siakaramalegos@gmail.com>
2016-02-19 16:34:41 -0600

    Adds remaining feature specs and adds notice to secrets index

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,19 +1,52 @@
 require 'rails_helper'
 
+# How to think about breaking up into separate files and features/contexts/etc.?
 feature 'Users' do
 
   let (:user){ create(:user) }
 
   before do
+    sign_in(user)
+    create_list(:secret, 25)
+    sign_out
+
     visit root_path
   end
 
   context 'without sign-in' do
-    scenario 'allows viewing of all secrets'
-    scenario 'does not allow creation of a secret'
-    scenario 'does not allow editing a secret'
-    scenario 'does not allow deleting a secret'
-    scenario 'allows sign up'
+    scenario 'allows viewing of last five secrets' do
+      expect(page).to have_content("**hidden**", count: 5)
+    end
+
+    scenario 'does not allow creation of a secret' do
+      click_link 'New Secret'
+      expect(page).to have_content "Not authorized, please sign in!"
+    end
+
+    scenario 'does not allow editing a secret' do
+      visit edit_secret_path(Secret.last)
+      expect(page).to have_content "Not authorized, please sign in!"
+    end
+
+    scenario 'does not allow deleting a secret' do
+      page.driver.submit :delete, "/secrets/#{Secret.last.id}", {}
+      expect(page).to have_content "Not authorized, please sign in!"
+    end
+
+    scenario 'allows sign up' do
+      click_link 'All Users'
+      click_link 'New User'
+
+      fill_in 'Name', with: 'Foo'
+      fill_in 'Email', with: 'foo@bar.com'
+      fill_in 'Password', with: 'password'
+      fill_in 'Password confirmation', with: 'password'
+
+      click_button 'Create User'
+
+      expect(page).to have_content "User was successfully created."
+      expect(page).to have_content "Foo"
+    end
   end
 
   context 'with improper credentials' do
@@ -36,9 +69,42 @@ feature 'Users' do
       expect(page).to have_content "You've successfully signed in"
     end
 
-    scenario 'allows creation of a secret'
-    scenario 'allows editing of my secrets'
-    scenario 'allows deleting of my secrets'
-    scenario 'signs me out'
+    scenario 'signs me out' do
+      sign_out
+      expect(page).to have_content "You've successfully signed out"
+    end
+
+    context 'allows secret' do
+      before do
+        click_link 'New Secret'
+        fill_in 'Title', with: 'Lorem'
+        fill_in 'Body', with: "Ipsum"
+        click_button 'Create Secret'
+      end
+
+      scenario 'creation' do
+        expect(page).to have_content "Secret was successfully created."
+
+        # Should I put these only in controller/view tests?
+        expect(page).to have_content "Lorem"
+        expect(page).to have_content "Ipsum"
+        expect(page).to have_content user.name
+      end
+
+      scenario 'editing' do
+        click_link 'Edit'
+        fill_in 'Title', with: 'Another title'
+        click_button 'Update Secret'
+
+        expect(page).to have_content "Secret was successfully updated."
+      end
+
+      scenario 'deleting' do
+        visit root_path
+        click_link 'Destroy'
+
+        expect(page).to have_content "Secret was successfully destroyed."
+      end
+    end
   end
 end
