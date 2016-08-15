31b793133fe4c9c847331aa4cb744168574bf5b3
Hannah Squier <hannahsquier@gmail.com>
2016-08-15 10:58:29 -0700

    add secret model tests

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :user do
+  factory :user, aliases: [:author] do
     sequence(:name) { |n| "pokemon#{n}"}
     email { "#{name}@email.com" }
     password "foobar"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 describe User do
-  # Validations
 
+  let(:user){build(:user)}
+
+  # Validations
   describe "validations" do
-    let(:user){build(:user)}
 
     it "is valid with default attributes" do
       expect(user).to be_valid
@@ -18,18 +19,30 @@ describe User do
       new_user1 = build(:user, :name => "a" * 21)
       expect(new_user1.valid?).to eq(false)
       new_user2 = build(:user, :name => "a" * 2)
-      expect(new_user2.valid?).to eq(false)  
+      expect(new_user2.valid?).to eq(false)
+    end
+
+    it "is not vaild if password length is greater than 16 or less than 6" do
+      new_user1 = build(:user, :password => "a" * 17)
+      expect(new_user1.valid?).to eq(false)
+      new_user2 = build(:user, :password => "a" * 5)
+      expect(new_user2.valid?).to eq(false)
     end
 
 
+    context "when saving multiple users" do
+      before { user.save! }
+
+      it "doesn't allow identical email addresses" do
+        new_user = build(:user, email: user.email)
+        expect(new_user.valid?).to eq(false)
+      end
+    end
   end
 
 
-  
-  
-  it "doesn't allow identical email addresses"
-  it "is not vaild if password length is greater than 16 or less than 6"
-
   # Associations
-  it "responds to secrets association"
+  it "responds to secrets association" do
+    expect(user).to respond_to(:secrets)
+  end
 end
