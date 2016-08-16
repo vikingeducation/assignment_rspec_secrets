81db654eadb2f941726567ef3c3700d3fe903706
cjvirtucio87 <cjv287@gmail.com>
2016-08-16 09:20:13 -0700

    Working on users spec

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,1 +1,9 @@
-feature 
+require 'rails_helper'
+
+feature 'Users' do
+  let!(:user) { create(:user) }
+  let!(:secret) { user.secrets.create(attributes_for(:secret)) }
+
+  it "signs up the user"
+
+end
