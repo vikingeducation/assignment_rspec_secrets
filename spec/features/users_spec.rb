bab08d619df66a2499149c2a390f90635d13d883
jsteenb2 <jonathansteenbergen@gmail.com>
2016-08-16 09:33:13 -0700

    added 15 tests, all 15

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -41,7 +41,117 @@ feature 'User' do
     expect(page).to have_content "Secret title"
   end
 
+  scenario "edit one of previously created secrets" do
+    sign_in(user)
+    create_secret
+    click_link "All Secrets"
+    click_link "Edit"
+    fill_in "Title", with: "New title"
+    fill_in "Body", with: "new body"
+    click_button "Update Secret"
 
+    expect(page).to have_content "Secret was successfully updated."
 
+    expect(page).to have_content "New title"
+    expect(page).to have_content "new body"
+  end
 
+  scenario "delete one of previously created secrets" do
+    sign_in(user)
+    create_secret
+    click_link "All Secrets"
+    click_link "Destroy"
+
+    expect(page).not_to have_content "Secret title"
+    expect(page).not_to have_content "Secret body"
+  end
+
+  scenario "should not see author if not logged in and secrets have been made" do
+    sign_in(user)
+    create_secret
+    sign_out
+    visit root_path
+    expect(page).to have_content "**hidden**"
+  end
+
+  scenario "should not show any secrets if none have been created" do
+    visit root_path
+    expect(page).not_to have_content "Show"
+  end
+
+  scenario "should not allow a user to delete a different users account" do
+    sign_in(user)
+    sign_out
+    visit root_path
+    click_link "All Users"
+    click_link "Destroy"
+    expect(page).to have_content "Password"
+    expect(page).to have_content "Email"
+  end
+
+  scenario "shouldn't be able to log in after user is already logged in" do
+    sign_in(user)
+    visit root_path
+
+    expect(page).to have_content "Logout"
+    expect(page).not_to have_content "Login"
+  end
+
+  scenario "should not be able to logout when not logged in" do
+    sign_in(user)
+    sign_out
+
+    visit root_path
+    expect(page).not_to have_content "Logout"
+    expect(page).to have_content "Login"
+  end
+
+  scenario "shouldn't be able to delete secrets when not signed in" do
+    sign_in(user)
+    sign_out
+
+    visit root_path
+    click_link "All Secrets"
+
+    expect(page).not_to have_content "Destroy"
+  end
+
+  scenario "should not be able to create a new secret when not signed in" do
+    sign_in(user)
+    sign_out
+
+    visit root_path
+    click_link "All Secrets"
+    click_link "New Secret"
+
+    expect(page).to have_content "Password"
+    expect(page).to have_content "Email"
+
+    expect(page).not_to have_content "Title"
+    expect(page).not_to have_content "Body"
+  end
+
+  scenario "shoud not be able to see users show page without signing in" do
+    sign_in(user)
+    sign_out
+
+    visit root_path
+    click_link "All Users"
+    click_link "Show"
+
+    expect(page).to have_content "Password"
+    expect(page).to have_content "Email"
+  end
+
+  scenario "shoud not be able to see users edit page without signing in" do
+    sign_in(user)
+    sign_out
+
+    visit root_path
+    click_link "All Users"
+    click_link "Edit"
+
+    expect(page).to have_content "Password"
+    expect(page).to have_content "Email"
+  end
 end
