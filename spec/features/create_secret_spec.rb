d132c47a9f6927df31933c5c67b00217f6fa0cff
Hannah Squier <hannahsquier@gmail.com>
2016-08-16 09:54:13 -0700

    add morgans branch

---
+++ spec/features/create_secret_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'as a signed in user want to create a secret' do
+  let(:user) { create(:user) }
+
+  scenario 'create secret' do
+    old_num_secrets = Secret.count
+    login(user)
+    click_link('New Secret')
+    fill_in('Title', with: 'My Secret Title')
+    fill_in('Body', with: 'My Secret Body')
+    click_button('Create Secret')
+    expect(Secret.count).to eq(old_num_secrets + 1)
+  end
+
+  scenario 'create secret without being logged in'
+end
