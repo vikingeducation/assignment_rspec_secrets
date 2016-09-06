d1c9b47e2c27c6afb5a566980b3aac46cbf89b83
BranLiang <lby89757@hotmail.com>
2016-09-06 11:06:59 +0800

    finish the feature test

---
+++ spec/support/login_macros.rb
@@ -0,0 +1,14 @@
+module LoginMacros
+  def sign_in(user)
+    visit root_path
+    click_link "Login"
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button "Log in"
+  end
+
+  def sign_out
+    visit root_path
+    click_link "Logout"
+  end
+end
