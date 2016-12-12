29731f7a3874188b918764c8225b442b3fd496c6
Kenneth Kottenstette <kennykotten@gmail.com>
2016-12-12 12:54:27 -0800

    added new secret validations

--- spec/models/secret_spec.rb
+++ spec/models/secret_spec.rb
@@ -5,6 +5,16 @@ describe Secret do
   let(:secret){ build(:secret) }
   let(:num_secrets){ 5 }
 
+  describe "Secret associations" do
+    it ("is linked to an author") do
+      expect(secret).to respond_to(:author)
+  end
+
+  it ("is linked to an author") do
+    user2 = build(:user)
+    expect(secret.id).to_not eq(user2)
+  end
+
   describe "#last_five"
     before do
       secrets = create_list(:secret, 7)
