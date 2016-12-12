917f526f244524af7108d2e0fd12234717094387
Dexter Ford <dexter.ford.77@gmail.com>
2016-12-12 17:06:59 -0600

    Done

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,11 +8,7 @@ FactoryGirl.define do
 
   factory :user, aliases: [:author] do
     sequence(:name){ |n| "Foo#{n}"}
-
-    # Don't forget to pass a BLOCK if you
-    #   are interpolating dynamic values:
     email { "#{name}@bar.com" }
-
     password "foobar123"
   end
 
--- spec/models/secret_spec.rb
+++ spec/models/secret_spec.rb
@@ -8,9 +8,9 @@ describe Secret do
     expect(secret).to be_valid
   end
 
-  it "is rejects secrets without body, title, or author" do
-    secret = create(:secret, title: "")
-    expect
+  it "rejects secrets without body or title" do
+    expect{create(:secret, title: "")}.to raise_error(ActiveRecord::RecordInvalid)
+    expect{create(:secret, body: "")}.to raise_error(ActiveRecord::RecordInvalid)
   end
 
   it "validates all its biz" do
@@ -19,9 +19,9 @@ describe Secret do
     should validate_length_of(:body)
   end
 
-  # it "belong to an author" do
-  #   should belong_to(:author)
-  # end
+  it "belongs to an author" do
+    should belong_to(:author)
+  end
 
   describe ".last_five" do
     it "returns the 5 most recent secrets" do
@@ -36,30 +36,3 @@ describe Secret do
   end
 
 end
-
-# Validation
-
-# describe Artist do
-#   let(:artist) { create(:artist) }
-
-#   # ----------------------------------------
-#   # Validations
-#   # ----------------------------------------
-
-#   it do
-#     should validate_presence_of(:name)
-#   end
-
-#   # ----------------------------------------
-#   # Associations
-#   # ----------------------------------------
-
-#   it do
-#     should have_many(:songs)
-#   end
-
-
-#   it do
-#     should have_many(:bookmarks).dependent(:destroy)
-#   end
-# end
