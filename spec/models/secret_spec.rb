a6df5e303043a84f36d943f2a217c8dd920dd7e4
Kenneth Kottenstette <kennykotten@gmail.com>
2016-12-12 13:53:16 -0800

    last few

--- spec/models/secret_spec.rb
+++ spec/models/secret_spec.rb
@@ -8,18 +8,18 @@ describe Secret do
   describe "Secret associations" do
     it ("is linked to an author") do
       expect(secret).to respond_to(:author)
+    end
   end
 
-  it ("is linked to an author") do
+  it "is linked to an author" do
     user2 = build(:user)
     expect(secret.id).to_not eq(user2)
   end
 
-  describe "#last_five"
-    before do
-      secrets = create_list(:secret, 7)
-    end
+  describe "#last_five" do
     it "returns a users last 5 secrets" do
-      expect(secret.last_five).to eq(num_secrets)
+      secrets = create_list(:secret, 7)
+      expect(Secret.last_five).to match_array(secrets[2..6])
     end
+  end
 end
