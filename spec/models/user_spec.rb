a035792b9dab84eab1a17c43805bde9267536748
Jonathan Popenuck <john.popenuck@gmail.com>
2016-12-12 14:54:02 -0800

    finished

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ describe User do
   let(:user){ build(:user) }
 
   describe "defaults" do
+
     it "is valid with default attributes" do
       expect(user).to be_valid
     end
@@ -11,9 +12,11 @@ describe User do
     it "saves with default attributes" do
       expect{user.save!}.not_to raise_error
     end
+
   end
 
   describe "invalid attributes" do
+
     it "requires a name longer than 2 characters" do
       different_user = build(:user, name: "Bo")
       expect(different_user).not_to be_valid
@@ -27,13 +30,16 @@ describe User do
   end
 
   describe "password" do
+
     it "requires a password longer than 5 characters" do
       different_user = build(:user, password: "12345" )
       expect(different_user).not_to be_valid
     end
+
   end
 
   describe "email uniqueness" do
+
     before do
       user.save!
     end
@@ -41,12 +47,15 @@ describe User do
       different_user = build(:user, email: user.email)
       expect{different_user.save!}.to raise_error
     end
+
   end
 
   describe "user associations" do
+
     it "responds to a call to secrets association" do
       expect(user).to respond_to :secrets
     end
+
   end
 
 end
