9f46745198007bfa42eb154b076e441c182e1bf5
Catherine Kwak <khopsickle@users.noreply.github.com>
2016-12-13 11:29:10 -0700

    Setup factories, start writing a few tests

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,17 +2,38 @@ require 'rails_helper'
 
 describe "Visitor actions" do
 
+  let ( :user ) { build(:user) }
+
   context "viewing secrets" do
 
-    it "can view all secrets"
+    it "can view all secrets" do
+      visit root_path
+      expect(page).to have_content "Listing secrets"
+    end
 
-    it "cannot see other users"
+    it "cannot see other user's show pages" do
+      user.save!
+      visit users_path
+      click_on "Show"
+      expect(page).to have_current_path(new_session_path)
+    end
 
   end
 
   context "signing up" do
+    before do
+      visit new_user_path
+      fill_in "Name", with: user.name
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
+      fill_in "Password confirmation", with: user.password
+    end
 
-    it "successfully adds a user for valid signups"
+    it "successfully adds a user for valid signups" do
+      click_on "Create User"
+      expect(page).to have_content("User was successfully created.")
+      # expect(page).to have_current_path(user_path(user))
+    end
 
     it "does not add a user for invalid signups"
 
