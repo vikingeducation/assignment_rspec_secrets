85808f0e6458ee792e89ad15c010adf8960e2fd6
Jonathan Popenuck <john.popenuck@gmail.com>
2016-12-13 12:02:55 -0800

    worked on sad and bad paths

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -17,13 +17,7 @@ feature 'Secrets#index' do
 
   context "sign up new user" do
     before do
-      click_link "All Users"
-      click_link "New User"
-      fill_in "Name", with: name
-      fill_in "Email", with: email
-      fill_in "Password", with: "password"
-      fill_in "Password confirmation", with: "password"
-      click_button "Create User"
+      create_user
     end
 
     it "allows signup" do
@@ -85,11 +79,90 @@ feature 'Secrets#index' do
       create_secret
       click_link "All Secrets"
       click_link "Destroy"
-      save_and_open_page
     end
 
     it "does soemthing" do
       expect(html).to_not include("<td>")
+    end
+
+  end
+
+  context "when creating a new user" do
+
+    before do
+      create_invalid_user
+    end
+
+    it "will show error messages when using an invalid input" do
+      expect(page).to have_content("prohibited this user from being saved:")
+    end
+
+    it "will style fields as fields with errors" do
+      expect(page).to have_css("div.field_with_errors")
+    end
+
+  end
+
+  context "while logged in" do
+
+    before do
+      sign_in(user)
+    end
+
+    it "will not allow you to create a new user" do
+      # unwanted behavior
+      create_user
+      expect(page).to_not have_content("User was successfully created.")
+    end
+
+    it "will allow you to attempt to delete another user" do
+      create_alternative_user
+      click_link "All Users"
+      expect(page).to have_content("Destroy", count: 2)
+    end
+
+    it "will allow you to attempt to edit another user" do
+      create_alternative_user
+      click_link "All Users"
+      expect(page).to have_content("Edit", count: 2)
+    end
+
+  end
+
+  context "when creating a new secret" do
+
+    before do
+      sign_in(user)
+      create_invalid_secret
+    end
+
+    it "will show error messages when using an invalid input" do
+      expect(page).to have_content("prohibited this secret from being saved:")
+    end
+
+    it "will style fields as fields with errors" do
+      expect(page).to have_css("div.field_with_errors")
+    end
+
+  end
+
+  context "when editing a secret" do
+    before do
+      sign_in(user)
+      create_secret
+      click_link "All Secrets"
+      click_link "Edit"
+      fill_in "Title", with: ""
+      fill_in "Body", with: ""
+      click_button "Update Secret"
+    end
+
+    it "will show error messages when using an invalid input" do
+      expect(page).to have_content("prohibited this secret from being saved:")
+    end
+
+    it "will style fields as fields with errors" do
+      expect(page).to have_css("div.field_with_errors")
     end
 
   end
