0929ad6eccb02bd70a3b5af7236ec7e0aeadbd54
tt <ttwang.303@gmail.com>
2017-01-29 22:50:02 -0500

    add edge cases

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -76,68 +76,111 @@ end
 
 feature 'Create a Secret' do
   let(:user){ create(:user) }
-  before do
-    sign_in(user)
+
+  context 'User signs in' do
+    before do
+      sign_in(user)
+    end
+
+    scenario 'successfully creates a secret' do
+      visit(root_path)
+      click_link('New Secret')
+      fill_in 'Title', with: 'title'
+      fill_in 'Body', with: 'body'
+      click_button('Create Secret')
+      expect(page).to have_content('Secret was successfully created.')
+    end
+
+    scenario 'display errors when form is empty' do
+      visit(new_secret_path)
+      click_button('Create Secret')
+      expect(page).to have_content('4 errors prohibited this secret from being saved:')
+    end
   end
 
-  scenario 'successfully creates a secret' do
-    visit(root_path)
-    click_link('New Secret')
-    fill_in 'Title', with: 'title'
-    fill_in 'Body', with: 'body'
-    click_button('Create Secret')
-    expect(page).to have_content('Secret was successfully created.')
+  context 'User not signed in' do
+    scenario 'cannot access new secret form' do
+      visit(root_path)
+      click_link("New Secret")
+      expect(current_path).to eq(new_session_path)
+    end
   end
 
-  scenario 'display errors when form is empty' do
-    visit(new_secret_path)
-    click_button('Create Secret')
-    expect(page).to have_content('4 errors prohibited this secret from being saved:')
-  end
 end
 
 feature 'Edit a Secret' do
   let(:user){ create(:user) }
-  before do
-    sign_in(user)
-    signed_in_user_creates_secret
-    visit(root_path)
-    click_link('Edit')
+
+  context 'Signed in user creates secret' do 
+    before do
+      sign_in(user)
+      signed_in_user_creates_secret
+      visit(root_path)
+      click_link('Edit')
+    end
+
+    scenario 'access edit form' do
+      expect(page).to have_content('Editing secret')
+    end
+
+    scenario 'successfully edit a secret' do
+      fill_in 'Title', with: 'New Title'
+      fill_in 'Body', with: 'New Body'
+      click_button('Update Secret')
+      expect(page).to have_content('Secret was successfully updated.')
+    end
+
+    scenario 'display errors when invalid content submitted' do
+      fill_in 'Title', with: ''
+      fill_in 'Body', with: ''
+      click_button('Update Secret')
+      expect(page).to have_content('4 errors prohibited this secret from being saved:')
+    end
   end
 
-  scenario 'access edit form' do
-    expect(page).to have_content('Editing secret')
-  end
+  context "Signed in user with no secrets" do
+    before do
+      sign_in(user)
+    end
 
-  scenario 'successfully edit a secret' do
-    fill_in 'Title', with: 'New Title'
-    fill_in 'Body', with: 'New Body'
-    click_button('Update Secret')
-    expect(page).to have_content('Secret was successfully updated.')
-  end
-
-  scenario 'display errors when invalid content submitted' do
-    fill_in 'Title', with: ''
-    fill_in 'Body', with: ''
-    click_button('Update Secret')
-    expect(page).to have_content('4 errors prohibited this secret from being saved:')
+    scenario "can't edit other people's secrets" do
+      create_list(:secret, 10)
+      visit(secrets_path)
+      expect(page).not_to have_content('Edit')
+    end
   end
 end
 
 feature 'Delete a Secret' do
   let(:user){ create(:user) }
-  before do
-    sign_in(user)
-    signed_in_user_creates_secret
-    visit(root_path)
+
+  context 'Signed in user creates secret' do 
+    before do
+      sign_in(user)
+      signed_in_user_creates_secret
+      visit(root_path)
+    end
+
+    scenario 'access delete link' do
+      expect(page).to have_content('Destroy')
+    end
+
+    scenario 'successfully deletes link' do
+      expect{ click_link('Destroy') }.to change{ Secret.count }.by(-1)
+    end
   end
 
-  scenario 'access delete link' do
-    expect(page).to have_content('Destroy')
-  end
 
-  scenario 'successfully deletes link' do
-    expect{ click_link('Destroy') }.to change{ Secret.count }.by(-1)
+  context 'Signed in user has no secrets' do
+    before do
+      sign_in(user)
+    end
+
+    scenario "can't delete other people's secrets" do
+      create_list(:secret, 10)
+      visit(secrets_path)
+      expect(page).not_to have_content('Destroy')
+    end
   end
 
 end
