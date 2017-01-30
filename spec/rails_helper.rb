24fad2e328d2526d3376448b6ef79504024ae1ae
tt <ttwang.303@gmail.com>
2017-01-29 22:36:58 -0500

    happy integration tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,10 +3,15 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+# Add additional requires below this line. Rails is not loaded until this point!
 require 'spec_helper'
 require 'rspec/rails'
 require 'factory_girl_rails'
-# Add additional requires below this line. Rails is not loaded until this point!
+require 'capybara/rails'
+
+
+
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -21,7 +26,7 @@ require 'factory_girl_rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -58,5 +63,6 @@ RSpec.configure do |config|
 
   RSpec.configure do |config|
     config.include FactoryGirl::Syntax::Methods
+    config.include LoginMacros
   end
 end
