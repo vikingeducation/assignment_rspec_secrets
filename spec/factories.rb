6156471f3855eed73d9d862bd09a2e1a3e23b080
Visiona <darbis@gmail.com>
2017-07-07 12:06:58 +0100

    extra cleaning

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,27 +5,19 @@ FactoryGirl.define do
     sequence(:email) { |n| "#{name}_#{n}@factory.com" }
     # email { "#{name}@gmail.com" }
     password "loko12"
+
+    # trait :frequent_author do
+    #   after_build do |user|
+    #     create_list( :secret, 5, author: user )
+    #   end
+    # end
+
   end
 
   factory :secret do
     sequence(:title){ |n| "Hey, my title - #{n}"}
     sequence(:body){ |n| "Hey, my new body - #{n}"}
     author
-
-
-    # trait :frequent_author do
-    #   after_build do |user|
-    #     create_list( :post, 5, author: user )
-    #   end
-    # end
-
-    # trait :recent do
-    #   created_at { Time.now }
-    # end
-    #
-    # trait :old do
-    #   created_at { 1.week.ago }
-    # end
 
   end
 
