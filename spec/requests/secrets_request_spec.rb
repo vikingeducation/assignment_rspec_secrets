bcdeb62f0ce0ee9554634cad7d498e5825b5b319
Visiona <darbis@gmail.com>
2017-07-15 16:00:54 +0100

    Added tests verifying that authorized users can perform actions

--- spec/requests/secrets_request_spec.rb
+++ spec/requests/secrets_request_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
+require 'pry'
 
 describe 'SecretRequests' do
 
   let(:user){create(:user)}
+  let(:another_user){create(:user)}
   let(:secret){create(:secret, author: user)}
+  let(:another_secret){create(:secret, author: another_user)}
 
   before do
     secret
+    another_secret
   end
 
   describe 'GET #show' do
@@ -19,23 +23,47 @@ describe 'SecretRequests' do
   end
 
   describe "PATCH #edit" do
-    it "authorized users can edit their secret" do
 
+    let(:new_title_name){"New Top Secret"}
+
+    before do
+      login_as(user)
     end
 
-    it "unauthorized users cannot edit someone else's secret" do
+    context "authorized users can edit their secret" do
+      it "edit the secret" do
+        patch secret_path(secret), params: {:secret => attributes_for(:secret, :title => new_title_name) }
+        secret.reload
+        expect(secret.title).to eq(new_title_name)
+      end
 
+      it "redirects to the show secret" do
+       patch secret_path(secret), params: {:secret => attributes_for(:secret) }
+       expect(response).to redirect_to secret_path
+      end
     end
+
+      it "unauthorized users cannot edit someone else's secret" do
+
+      end
+
   end
 
 
   describe "DELETE #destroy" do
-    it "authorized users can delete their secret" do
 
+    before do
+      login_as(user)
+    end
+    # current_user.secrets.find(params[:id])
+    it "authorized users can delete their secret" do
+      expect{ delete secret_path(secret)}.to change(Secret, :count).by(-1)
     end
 
     it "unauthorized users cannot delete someone else's secret" do
-
+      RSpec::Expectations.configuration.on_potential_false_positives = :nothing
+      expect{ delete secret_path(another_secret)}.to raise_error
+      expect(response).to have_http_status(302)
     end
   end
 
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe 'UserRequests' do
 
+  let(:user){create(:user)}
+  let(:another_user){create(:user)}
+  let(:new_user_name){"My_new_name"}
+
   describe "POST #create" do
 
       let(:another_user_name){"s"}
@@ -17,8 +21,6 @@ describe 'UserRequests' do
    end
 
    describe "PATCH #edit" do
-     let(:user){create(:user)}
-     let(:another_user){create(:user)}
 
      before do
        user
@@ -27,7 +29,9 @@ describe 'UserRequests' do
      end
 
      it "authorized users can edit their profile" do
-
+       patch user_path(user), params: {:user => attributes_for(:user, :name => new_user_name) }
+       user.reload
+       expect(user.name).to eq(new_user_name)
      end
 
      it "unauthorized users cannot edit someone else's profile" do
@@ -37,9 +41,23 @@ describe 'UserRequests' do
 
 
    describe "DELETE #destroy" do
-     it "authorized users can delete their profile" do
 
+     before do
+       user
+       another_user
+       login_as(user)
      end
+
+     context "authorized users can delete their profile" do
+       it "destroys the user" do
+         expect{ delete user_path(user)}.to change(User, :count).by(-1)
+       end
+
+       it "redirects to the root" do
+        delete user_path(user)
+        expect(response).to redirect_to users_url
+      end
+    end
 
      it "unauthorized users cannot delete someone else's profile" do
 
