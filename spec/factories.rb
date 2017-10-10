eae615d8b32e658e43af5dafa3250da7e4b8ce94
Roy Chen <roychenlei@gmail.com>
2017-10-10 09:36:03 +0800

    create User factory

---
+++ spec/factories.rb
@@ -0,0 +1,12 @@
+# spec/factories.rb
+
+FactoryGirl.define do
+
+  factory :user do
+    name "Foo Bar"
+    email "foo@bar.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+
+end
