b0ca98c4cb750eb66ed841d6309cc679991d151e
Roy Chen <roychenlei@gmail.com>
2017-10-10 10:04:03 +0800

    create Secret factory, update User factory to use alias and sequences

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,11 +2,28 @@
 
 FactoryGirl.define do
 
-  factory :user do
-    name "Foo Bar"
-    email "foo@bar.com"
+  factory :user, aliases: [:author] do
+    first_name = "Foo"
+    last_name = "Bar"
+
+    sequence(:name) do |n|
+      "#{first_name} #{last_name} #{n}"
+    end
+
+    sequence(:email) do |n|
+      "#{first_name}.#{n}@bar.com".downcase
+    end
+
     password "foobar"
     password_confirmation "foobar"
+
+  end
+
+  factory :secret do
+    title "This is a secret"
+    body "A very, very secret secret. Shhhh."
+
+    author
   end
 
 end
