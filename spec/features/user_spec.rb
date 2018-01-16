2322d0c0fa0176097aab13b02b72ddab17c0256d
anne <richardson.ae@gmail.com>
2018-01-15 21:44:32 -0600

    Adds sad paths for object creation and editing

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -6,22 +6,37 @@ RSpec.feature User, type: :feature do
   end
 
   context 'Visitors' do
-    scenario "As a visitor, I want to view all secrets" do
-      expect(page).to have_selector('#spec_secrets_table')
-    end
+    context "As a visitor" do
+      scenario "I want to view all secrets" do
+        expect(page).to have_selector('#spec_secrets_table')
+      end
 
-    scenario "As a visitor, I want to sign up" do
-      click_link('Sign up')
+      scenario "I want to sign up" do
+        click_link('Sign up')
 
-      name = 'foo'
-      fill_in('Name', with: name)
-      fill_in('Email', with: "#{name}#@email.com")
-      fill_in('Password', with: 'password')
-      fill_in('Password confirmation', with: 'password')
+        name = 'foo'
+        fill_in('Name', with: name)
+        fill_in('Email', with: "#{name}#@email.com")
+        fill_in('Password', with: 'password')
+        fill_in('Password confirmation', with: 'password')
 
-      expect{ click_button("Create User") }.to change(User, :count).by(1)
-      expect(page).to have_content(name)
-      expect(page).to have_content("User was successfully created")
+        expect{ click_button("Create User") }.to change(User, :count).by(1)
+        expect(page).to have_content(name)
+        expect(page).to have_content("User was successfully created")
+      end
+
+      scenario "I want to be notified of form problems when signing up" do
+        click_link('Sign up')
+
+        name = 'f'
+        fill_in('Name', with: name)
+        fill_in('Email', with: "#{name}#@email.com")
+        fill_in('Password', with: 'password')
+        fill_in('Password confirmation', with: 'password')
+
+        click_button("Create User")
+        expect(page).to have_content('too short')
+      end
     end
   end
 
@@ -51,6 +66,20 @@ RSpec.feature User, type: :feature do
         expect(page).to have_content("Secret was successfully created")
       end
 
+      scenario "I want to be notified of form issues when creating a secret" do
+        login(user)
+
+        visit secrets_path
+        expect(page).to have_content('New Secret')
+        click_link('New Secret')
+
+        title = generate_string(1)
+        fill_in('Title', with: title )
+        fill_in('Body', with: generate_string(130))
+        click_button("Create Secret")
+        expect(page).to have_content('too short')
+      end
+
       scenario "I want to be able to edit one of my secrets" do
         login(user)
         secret = create_secret(user)
@@ -66,6 +95,21 @@ RSpec.feature User, type: :feature do
 
         expect(page).to have_content('Secret was successfully updated')
         expect(page).to have_content(changed_title)
+      end
+
+      scenario "I want to be notified of form issues when editing a secret" do
+        login(user)
+        secret = create_secret(user)
+        visit root_path
+
+        click_link('Edit')
+        expect(current_path).to eq(edit_secret_path(secret))
+        expect(page).to have_content('Edit')
+
+        changed_title = generate_string(1)
+        fill_in('Title', with: changed_title)
+        click_button('Update Secret')
+        expect(page).to have_content('too short')
       end
 
       scenario "I want to be able to delete one of my secrets" do
